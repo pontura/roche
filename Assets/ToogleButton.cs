31b8342598e1d40c5e9e306c1f9fe5ff1dd36554
Agustin Pontura <pontura@gmail.com>
2019-11-22 14:28:38 -0300

    popups

---
+++ Assets/ToogleButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToogleButton : MonoBehaviour
+{
+    public GameObject[] buttons;
+    public GameObject[] contents;
+    int id = -1;
+    Animation anim;
+    public AnimationClip[] clips;
+    void Awake()
+    {
+        anim = GetComponent<Animation>();
+        foreach (GameObject go in contents)
+        {
+            HandWriting handWritting = go.AddComponent(typeof(HandWriting)) as HandWriting;
+            handWritting.Init(go.GetComponentInChildren<Text>().text);
+        }
+    }
+    void OnEnable()
+    {
+        id = -1;
+        Clicked(0);
+    }
+    public void ToggleOneButton()
+    {
+        int _id;
+        if (id == 0)
+            _id = 1;
+        else
+            _id = 0;
+        Clicked(_id);
+    }
+    public void Clicked(int _id)
+    {
+        if (id == _id)
+            return;
+        this.id = _id;
+        print(id);
+        SetSelect();
+        if (anim != null)
+        {
+           anim.Play(clips[id].name);
+        }
+    }
+    void SetSelect()
+    {
+        Reset();
+        buttons[id].SetActive(true);
+        contents[id].SetActive(true);
+
+        contents[id].GetComponent<HandWriting>().CheckToHandwrite(contents[id]);
+    }
+    private void Reset()
+    {
+        foreach (GameObject go in buttons)
+            go.SetActive(false);
+        foreach (GameObject go in contents)
+            go.SetActive(false);
+
+    }
+}
